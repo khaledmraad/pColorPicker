6fd92f3ee4cf162a3a24f2fd1dd5b8c25e5ec5c9
khaledmraad <khaled.mrad@ensi-uma.tn>
2023-11-27 22:21:36 -0500

    pick from here

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import colorchooser
 
-window = Tk()
-window.title('Tkinter Color Chooserr')
-window.geometry('300x150')
+def pick_color():
+    color = colorchooser.askcolor()[1]  
+    if color:
+        color_label.config(foreground=color)  
+
+window = tk.Tk()
+window.title("Python Color Picker")
+
+color_label = tk.Label(window, text="this is your color", font=("Helvetica", 18))
+color_label.pack(pady=20)
+
+color_picker = tk.Button(window, text="Pick a Color", command=pick_color)
+color_picker.pack()
+
 
 window.mainloop()
